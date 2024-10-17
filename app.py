af9bb29f30962e8fced29cc83a360e246958458c
Vimlendu Sharma <159767918+VimlenduSharma@users.noreply.github.com>
2024-10-17 16:42:50 +0000

    added the flask project

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Hello, World!")
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+def hello_world():
+  return "Hello, World!"
+print(__name__)
+if __name__=="__main__":
+  app.run(host="0.0.0.0",debug=True)
